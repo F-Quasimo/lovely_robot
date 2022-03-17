56c7a655788d6478958c8d9066444878cfc7e338
notebook <frankensteinquasimo@outlook.com>
2022-03-18 00:18:58 +0800

    raspberry videocapture fix bug

---
+++ TAG_commit_Log.md
@@ -0,0 +1,12 @@
+
+
+commit f0f5c11d1aac19c9126a7c2ec137313e1214d63d (HEAD -> main)
+Author: notebook <frankensteinquasimo@outlook.com>
+Date:   Fri Mar 18 00:18:58 2022 +0800
+
+    raspberry videocapture fix bug
+describe:
+    raspberry opencamera cant fluent cv2.imshow~,and must add ".set(cv2.CAP_PROP_FOURCC,cv2.VideoWriter_fourcc('M','J','P','G'))"to decode stream. use 
+    v4l2-ctl -d /dev/video0 --all
+    v4l2-ctl -d /dev/video2 --list-formats
+    for more info.
--- tools/camera_mode.py
+++ tools/camera_mode.py
@@ -15,6 +15,7 @@ class SingleCam:
     def OpenCam(self):
         self.cap = cv2.VideoCapture(self.cam_id + self.cap_open_mode)
         self.cap.open(self.cam_id)
+        self.cap.set(cv2.CAP_PROP_FOURCC,cv2.VideoWriter_fourcc('M','J','P','G'))
         self.cap.set(cv2.CAP_PROP_FRAME_WIDTH, self.cam_size[0])
         self.cap.set(cv2.CAP_PROP_FRAME_HEIGHT, self.cam_size[1])
         print('DEBUG SingleCam OpenCam')
@@ -50,10 +51,12 @@ class StereoCam:
     def OpenCam(self):
         self.cap0 = cv2.VideoCapture(self.cam_id0 + self.cap_open_mode)
         self.cap0.open(self.cam_id0)
+        self.cap0.set(cv2.CAP_PROP_FOURCC,cv2.VideoWriter_fourcc('M','J','P','G'))
         self.cap0.set(cv2.CAP_PROP_FRAME_WIDTH, self.cam_size[0])
         self.cap0.set(cv2.CAP_PROP_FRAME_HEIGHT, self.cam_size[1])
         self.cap1 = cv2.VideoCapture(self.cam_id1 + self.cap_open_mode)
         self.cap1.open(self.cam_id1)
+        self.cap1.set(cv2.CAP_PROP_FOURCC,cv2.VideoWriter_fourcc('M','J','P','G'))
         self.cap1.set(cv2.CAP_PROP_FRAME_WIDTH, self.cam_size[0])
         self.cap1.set(cv2.CAP_PROP_FRAME_HEIGHT, self.cam_size[1])
         if self.cap0.isOpened() and self.cap1.isOpened():
@@ -79,3 +82,37 @@ class StereoCam:
             if self.cap1.isOpened():
                 self.cap1.release()
                 self.cap1 = None
+
+if __name__ == '__main__':
+    '''
+    sigcam = SingleCam(cam_id=2)
+    sigcam.OpenCam()
+    while True:
+        if not sigcam.cap.isOpened():
+            cv2.waitKey(3)
+            print('wait...\n')
+            continue
+        snap0=sigcam.SnapShoot()
+        if snap0 is None:
+            print('NONE OF IMG')
+            continue
+        snap0=cv2.resize(snap0, (512,288))
+        cv2.imshow('snap0', snap0)
+        cv2.imwrite('/home/pi/github/save.jpg', snap0)
+    '''
+    stereo_cam = StereoCam(cam_id0=2, cam_id1=0)
+    stereo_cam.OpenCam()
+    while True:
+        if not stereo_cam.cap1.isOpened():
+            cv2.waitKey(3)
+            print('wait...\n')
+            continue
+        snap0, snap1=stereo_cam.SnapShoot()
+        snap0=cv2.resize(snap0, (512,288))
+        snap1=cv2.resize(snap1, (512,288))
+        cv2.imshow('snap0', snap0)
+        cv2.imshow('snap1', snap1)
+        cv2.imwrite('/home/pi/github/save00.jpg', snap0)
+        cv2.imwrite('/home/pi/github/save01.jpg', snap1)
+        
+    
